bff3c38575ff9e4a71f78965a84691d4f0d79e15
Rob <rob@hattv.com>
2023-07-30 18:49:02 -0700

    Project files

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# uint-to-string
-Benchmarking: Convert uint256 to String in Solidity
+# The Fastest Way To Convert uint256 to String in Solidity
+
+This is a demo repo for the Medium article [The Fastest Way To Convert uint256 to String in Solidity]().
+
+Relevant code is in index.js. This repo is MIT licensed, use at your own risk.
+
+The following algorithms are included here and fall under OpenZeppelin and Stack Overflow licenses and terms of service, accordingly.
+
+1. OpenZepplin - [[Strings.sol](https://github.com/OpenZeppelin/openzeppelin-contracts/blob/d6b63a48ba440ad8d551383697db6e5b0ef84137/contracts/utils/Strings.sol#L24)]()
+2. [https://stackoverflow.com/questions/47129173/how-to-convert-uint-to-string-in-solidity/65707309#65707309](https://stackoverflow.com/questions/47129173/how-to-convert-uint-to-string-in-solidity/65707309#65707309)
+3. [https://stackoverflow.com/questions/47129173/how-to-convert-uint-to-string-in-solidity/71095692#71095692](https://stackoverflow.com/questions/47129173/how-to-convert-uint-to-string-in-solidity/71095692#71095692)
+
+Smart contract deployed on Mumbai testnet at `0xb51a3175aCcE7D01bFD0717f9C4BD69a13dF6D3C`
+
+## Setup
+
+1. Download the project repo
+2. Run `npm install` from a terminal, at the root of the project
+
+## Run the demo
+
+1. Run `npm run start` or `node index.js`
+
+## Results data
+
+Output from this test can be reviewed here:
+[https://docs.google.com/spreadsheets/d/1hz_kDJj67geRG1vs_I2ks9evDmkEaflIrczzTkPynNI/edit?usp=sharing](https://docs.google.com/spreadsheets/d/1hz_kDJj67geRG1vs_I2ks9evDmkEaflIrczzTkPynNI/edit?usp=sharing)
---
+++ index.js
@@ -0,0 +1,102 @@
+import { ethers } from 'ethers'
+import ABI from './abi.json' assert { type: 'json' }
+
+//  Sign up for a free API key from https://www.alchemy.com
+const PROVIDER = new ethers.AlchemyProvider('matic-mumbai', 'oUyr_vSEz6W9t0FbwdxP0mBpA_5ziquT')
+const WALLET_PKEY = '0xb5537bcfe058b4943381b9f06e1ef47a516bd0ce2384def482883ef645444ede'
+const WALLET = new ethers.Wallet(WALLET_PKEY, PROVIDER)
+
+// https://mumbai.polygonscan.com/address/0xb51a3175aCcE7D01bFD0717f9C4BD69a13dF6D3C#code
+const CONTRACT = new ethers.Contract('0xb51a3175aCcE7D01bFD0717f9C4BD69a13dF6D3C', ABI, WALLET)
+
+const METHODS = [
+    'uintToStringProvable',
+    'uintToStringOZ',
+    'uintToStringMikhail',
+    'uintToStringABI'
+]
+
+const INTS = [
+    0n,
+    1n,
+    2n,
+    100n,
+    1234567890n,
+    (2n ** 32n) - 1n,
+    (2n ** 32n),
+    (2n ** 32n) + 1n,
+    (2n ** 48n) - 1n,
+    (2n ** 48n),
+    (2n ** 48n) + 1n,
+    (2n ** 64n) - 1n,
+    (2n ** 64n),
+    (2n ** 64n) + 1n,
+    (2n ** 128n) - 1n,
+    (2n ** 128n),
+    (2n ** 128n) + 1n,
+    (2n ** 256n) - 1n,
+    (2n ** 256n)
+]
+
+let csv = 'algorithm,'
+INTS.map(i => csv += i.toString(10) + ',')
+
+let count = 0
+for(let t = 0; t < METHODS.length; ++t) {
+    let method = METHODS[t]
+    csv += `\n${METHODS[t]},`
+    for(let i = 0; i < INTS.length; ++i) {
+        let input = INTS[i].toString(10)
+        const transactionDetails = await getTransactionPropertiesViaAlchemyRPC()
+        let receipt = undefined
+        console.log(`\nTEST ${++count}: ${method}(${input})`)
+        try {
+            const transaction = await CONTRACT[method](input, transactionDetails)
+            receipt = await transaction.wait()
+            console.log(`    https://mumbai.polygonscan.com/tx/${receipt.hash}`)
+            receipt.logs.map(raw => {
+                const log = CONTRACT.interface.parseLog(raw)
+                if(log?.name  == "TestResult") {
+                    if(log.args[0].toString(10) !== input) {
+                        console.log(`    ERROR: Input value mutated. Sent ${input} but contract received ${log.args[0].toString(10)}`)
+                        console.log(`    ${input} != ${log.args[0].toString(10)} ?? ${log.args[1]}`)
+                        csv += 'ERROR,'
+                    } else if(log.args[0].toString(10) !== log.args[1]) {
+                        console.log(`    ERROR: Encoding mismatch: ${log.args[0].toString(10)} != ${log.args[1]}`)
+                        console.log(`    ${input} == ${log.args[0].toString(10)} == ${log.args[1]}`)
+                        csv += 'ERROR,'
+                    } else {
+                        console.log(`    ${input} == ${log.args[0].toString(10)} == ${log.args[1]}`)
+                        console.log(`    ${log.args[2]} gas used`)
+                        csv += `${log.args[2]},`
+                    }
+                }
+            })
+        } catch (err) {
+            console.log(`    ERROR: Transaction failed ${method}: ${input}`)
+            console.log(err)
+            csv += 'EXCEPTION,'
+        }
+    }
+}
+
+console.log('===================================================================\n')
+console.log('RESULTS:\n')
+console.log(csv)
+console.log()
+
+async function getTransactionPropertiesViaAlchemyRPC() {
+    let [block, priority_hex] = await Promise.all([
+        await PROVIDER.getBlock('latest'),
+        await PROVIDER.send('eth_maxPriorityFeePerGas', [])
+    ])
+
+    const base_fee = block.baseFeePerGas ?? 15
+    const priority = BigInt(priority_hex)
+    const base = BigInt((base_fee * 115n) / 100n)   //  +15% in case traffic rises
+
+    return {
+        maxPriorityFeePerGas: priority.toString(),
+        maxFeePerGas: (base + priority).toString(),
+    }
+}
